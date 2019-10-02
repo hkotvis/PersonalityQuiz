df4c2b4ea0520f4cae81b50444de255b91c2bc0a
hkotvis <hkotvis@my.wctc.edu>
2019-10-02 17:13:39 -0500

    Add project files.

---
+++ PersonalityQuiz/PersonalityQuiz/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿//using PersonalityQuiz.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PersonalityQuiz
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           // BindingContext = new QuizViewModel();
+        }
+        private async void goToResults(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Results());
+        }
+    }
+}
---
+++ PersonalityQuiz/PersonalityQuiz/ViewModels/QuizViewModel.cs
@@ -0,0 +1,42 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.ComponentModel;
+//using Xamarin.Forms;
+
+//namespace PersonalityQuiz.ViewModels
+//{
+//    class QuizViewModel
+//    {
+//        public QuizViewModel()
+//        {
+//            CharacterCommand = new Command(whichCharacter);
+//        }
+        
+//        public string Name
+//        {
+//            get { return Name; }
+//            set { Name = value; }
+//        }
+//        public int Age
+//        {
+//            get { return Age; }
+//            set { Age = value; }
+//        }
+    
+//        public Command CharacterCommand { get; }
+
+//        void whichCharacter()
+//        {
+//            try
+//            {
+                
+//            }
+//            catch
+//            {
+
+//            }
+//        }
+
+//    }
+//}
