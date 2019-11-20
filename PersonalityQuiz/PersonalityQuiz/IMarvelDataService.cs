2842858b3f388ab951e10921e5ecd60bdfc89768
hkotvis <hkotvis@my.wctc.edu>
2019-11-20 12:02:45 -0600

    marvel api

---
+++ PersonalityQuiz/PersonalityQuiz/IMarvelDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalityQuiz
+{
+    public interface IMarvelDataService
+    {
+        Task<IEnumerable<Comic>> GetComicsBySeries(int seriesId, string orderBy = null);
+    }
+}
